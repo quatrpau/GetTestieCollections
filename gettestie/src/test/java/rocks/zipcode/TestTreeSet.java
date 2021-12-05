d057f048ed7318e82f6b565c388d20859ad59d58
Quatrani Paul <quatranipaul@zipcodes-MacBook-Pro-9.local>
2021-12-05 14:28:08 -0500

    finished treeset

--- gettestie/src/test/java/rocks/zipcode/TestTreeSet.java
+++ gettestie/src/test/java/rocks/zipcode/TestTreeSet.java
@@ -5,47 +5,55 @@ package rocks.zipcode;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.TreeSet;
 
 import static org.junit.Assert.*;
 
 public class TestTreeSet {
-    private TreeSet<String> treeSet;
+    private TreeSet<Integer> treeSet;
     @Before
     public void setUp() throws Exception {
         //given
         treeSet = new TreeSet<>();
     }
     @Test
-    public void TestStackPush() {
+    public void TestAdd() {
         //when
-        treeSet.push("Hello world");
+        treeSet.add(1);
         //then
         assertFalse(treeSet.isEmpty()); // false
     }
     @Test
-    public void TestStackPop() {
-        treeSet.push("Hello World");
+    public void TestRemove() {
+        treeSet.add(1);
         //when
-        treeSet.pop();
+        treeSet.remove(1);
         //then
         assertTrue(treeSet.isEmpty());
     }
 
     @Test
-    public void TestStackIsEmpty() {
+    public void TestIsEmpty() {
         //when^
         //then
         assertTrue(treeSet.isEmpty());
     }
 
     @Test
-    public void TestStackPeek() {
+    public void TestContains() {
         //when
-        String expected = "h";
-        treeSet.push("h");
-        assertEquals(expected, treeSet.peek());
-        assertFalse(treeSet.isEmpty());
+        Integer expected = 1;
+        treeSet.add(expected);
+        assertTrue(treeSet.contains(expected));
+
+    }
+    @Test
+    public void TestOrdering(){
+        Integer one = 1;
+        Integer zero = 0;
+        treeSet.addAll(Arrays.asList(one,zero));
+        assertEquals(zero,treeSet.pollFirst());
 
     }
     // Make a bigger test exercising more Stack methods.....
